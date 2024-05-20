6204b63f58147091e57d38600b83fc385f08c362
Farron <asuxolovskij@mail.ru>
2024-05-20 15:48:24 +0300

    Changes to be committed: 	modified:   OSg.lua

--- OSg.lua
+++ OSg.lua
@@ -1,16 +1,9 @@
-local items = {
-    {name = "Folder1", type = "folder"},
-    {name = "File1.txt", type = "file"},
-    {name = "Folder2", type = "folder"},
-    {name = "File2.txt", type = "file"}
-}
-
-local function drawMainScreen()
+local function drawFilesAndFolders(files)
     term.setBackgroundColor(colors.blue)
     term.clear()
     term.setCursorPos(1, 1)
     term.setTextColor(colors.white)
-    for i, item in ipairs(items) do
+    for i, item in ipairs(files) do
         term.setCursorPos(1, i)
         print(item.name)
     end
@@ -25,18 +18,26 @@ local function drawContextMenu(x, y)
     end
 end
 
-local function handleMouseClick(x, y, button)
-    if button == 2 then
-        drawContextMenu(x, y)
-    end
-end
-
 local function main()
-    drawMainScreen()
+    local files = {
+        {name = "Folder1", type = "folder"},
+        {name = "File1.txt", type = "file"},
+        {name = "Folder2", type = "folder"},
+        {name = "File2.txt", type = "file"}
+    }
+
+    drawFilesAndFolders(files)
 
     while true do
         local event, button, x, y = os.pullEvent("mouse_click")
-        handleMouseClick(x, y, button)
+        if button == 2 then
+            drawContextMenu(x, y)
+            os.pullEvent("mouse_click")
+            term.setBackgroundColor(colors.blue)
+            term.clear()
+            term.setCursorPos(1, 1)
+            drawFilesAndFolders(files)
+        end
     end
 end
 
