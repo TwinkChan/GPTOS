270cf959dc6b113fb43555e250c5dcd002c7ba9b
Farron <asuxolovskij@mail.ru>
2024-05-20 15:55:22 +0300

    Changes to be committed: 	modified:   OSg.lua

--- OSg.lua
+++ OSg.lua
@@ -1,42 +1,68 @@
-local function drawFilesAndFolders(files)
-    term.setBackgroundColor(colors.blue)
+local items = {
+    {name = "Folder1", type = "folder"},
+    {name = "File1.txt", type = "file"},
+    {name = "Folder2", type = "folder"},
+    {name = "File2.txt", type = "file"}
+}
+
+local function drawWindowFrame()
+    term.setBackgroundColor(colors.lightGray)
+    term.setTextColor(colors.black)
     term.clear()
     term.setCursorPos(1, 1)
-    term.setTextColor(colors.white)
-    for i, item in ipairs(files) do
-        term.setCursorPos(1, i)
-        print(item.name)
+    term.write("My Computer")
+    term.setCursorPos(1, 2)
+    term.write(string.rep("-", term.getSize()))
+    term.setCursorPos(1, 3)
+    term.write("|")
+    term.setCursorPos(1, 4)
+    term.write(string.rep("-", term.getSize()))
+    term.setCursorPos(1, term.getHeight())
+    term.write(string.rep("-", term.getSize()))
+end
+
+local function drawFilesAndFolders()
+    term.setBackgroundColor(colors.white)
+    term.setTextColor(colors.black)
+    for i, item in ipairs(items) do
+        term.setCursorPos(3, i + 3)
+        if item.type == "folder" then
+            term.setTextColor(colors.blue)
+            term.write("[" .. item.name .. "]")
+        else
+            term.setTextColor(colors.black)
+            term.write(item.name)
+        end
     end
 end
 
 local function drawContextMenu(x, y)
-    term.setBackgroundColor(colors.gray)
-    term.setTextColor(colors.white)
-    for i, option in ipairs({"Open", "Rename", "Copy", "Delete"}) do
-        term.setCursorPos(x, y + i - 1)
-        print(option)
-    end
+    term.setBackgroundColor(colors.white)
+    term.setTextColor(colors.black)
+    term.setCursorPos(x, y)
+    term.clearLine()
+    term.write("Open")
+    term.setCursorPos(x, y + 1)
+    term.clearLine()
+    term.write("Rename")
+    term.setCursorPos(x, y + 2)
+    term.clearLine()
+    term.write("Copy")
+    term.setCursorPos(x, y + 3)
+    term.clearLine()
+    term.write("Delete")
 end
 
 local function main()
-    local files = {
-        {name = "Folder1", type = "folder"},
-        {name = "File1.txt", type = "file"},
-        {name = "Folder2", type = "folder"},
-        {name = "File2.txt", type = "file"}
-    }
-
-    drawFilesAndFolders(files)
+    drawWindowFrame()
+    drawFilesAndFolders()
 
     while true do
         local event, button, x, y = os.pullEvent("mouse_click")
         if button == 2 then
             drawContextMenu(x, y)
             os.pullEvent("mouse_click")
-            term.setBackgroundColor(colors.blue)
-            term.clear()
-            term.setCursorPos(1, 1)
-            drawFilesAndFolders(files)
+            drawFilesAndFolders()
         end
     end
 end
