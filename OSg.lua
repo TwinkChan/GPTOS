ec6fca9c1696a73c8dcf30794d18f728bb6c386c
Farron <asuxolovskij@mail.ru>
2024-05-20 15:34:38 +0300

    Changes to be committed: 	modified:   OSg.lua

--- OSg.lua
+++ OSg.lua
@@ -1,16 +1,8 @@
-local mainMenu = {
-    "File",
-    "Edit",
-    "View",
-    "Options",
-    "Help"
-}
-
-local contextMenu = {
-    "Open",
-    "Rename",
-    "Copy",
-    "Delete"
+local items = {
+    {name = "Folder1", type = "folder"},
+    {name = "File1.txt", type = "file"},
+    {name = "Folder2", type = "folder"},
+    {name = "File2.txt", type = "file"}
 }
 
 local function drawMainScreen()
@@ -18,23 +10,23 @@ local function drawMainScreen()
     term.clear()
     term.setCursorPos(1, 1)
     term.setTextColor(colors.white)
-    for i, option in ipairs(mainMenu) do
+    for i, item in ipairs(items) do
         term.setCursorPos(1, i)
-        print(option)
+        print(item.name)
     end
 end
 
 local function drawContextMenu(x, y)
     term.setBackgroundColor(colors.gray)
     term.setTextColor(colors.white)
-    for i, option in ipairs(contextMenu) do
+    for i, option in ipairs({"Open", "Rename", "Copy", "Delete"}) do
         term.setCursorPos(x, y + i - 1)
         print(option)
     end
 end
 
-local function handleMouseClick(x, y)
-    if x == 1 then
+local function handleMouseClick(x, y, button)
+    if button == 2 then
         drawContextMenu(x, y)
     end
 end
@@ -44,7 +36,7 @@ local function main()
 
     while true do
         local event, button, x, y = os.pullEvent("mouse_click")
-        handleMouseClick(x, y)
+        handleMouseClick(x, y, button)
     end
 end
 
